3aefa409dd316e36c25a7f118943f9fbdd6a814b
fta <1018598742@qq.com>
2020-05-06 17:22:32 +0800

    add:添加查看sd卡信息测试

--- app/src/main/java/com/fta/base/MainActivity.kt
+++ app/src/main/java/com/fta/base/MainActivity.kt
@@ -1,18 +1,25 @@
 package com.fta.base
 
+import android.content.ContentResolver
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.view.Gravity
+import android.view.View
 import android.widget.Button
 import com.fta.base.sdcard.SdCardAc
+import com.qmuiteam.qmui.kotlin.onClick
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.*
+import org.jetbrains.anko.support.v4.nestedScrollView
 import org.jetbrains.anko.startActivity as jumpAc
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        initView()
+        setContentView(MainAcUi().createView(AnkoContext.create(this,this)))
+//        initView()
     }
 
     private fun initView() {
@@ -22,5 +29,34 @@ class MainActivity : AppCompatActivity() {
             jumpAc<SdCardAc>()
         }
         rootMain.addView(sdCardBt, -1, -1)
+    }
+
+    class MainAcUi : AnkoComponent<MainActivity> {
+        override fun createView(ui: AnkoContext<MainActivity>): View = ui.apply {
+            nestedScrollView {
+                verticalLayout {
+                    button{
+                        text = "SD卡信息"
+                    }.onClick {
+                        jumpAc<SdCardAc>()
+                    }
+
+                    val showDeviceInfo = textView{
+                        gravity = Gravity.CENTER
+                    }
+
+                    button{
+                        text = "设备信息"
+                    }.onClick {
+                        showDeviceInfo.text = Settings.Secure.getString(
+                            it.context.contentResolver,
+                            Settings.Secure.ANDROID_ID
+                        )
+                    }
+                }
+
+            }
+
+        }.view
     }
 }
